ae94fe0d862868b8209f3bc74daffaf4cee5932a
shuvajit gayen <shuvajitgayen57@gmail.com>
2024-09-23 18:42:19 +0530

    add new migration

--- myapp/blog/models.py
+++ myapp/blog/models.py
@@ -4,10 +4,18 @@ from django.utils import timezone
 # Create your models here.
 
 class Blogs(models.Model):
+    BLOG_TYPE = [
+        ('P', 'Personal'),
+        ('W', 'Work'),
+        ('H', 'Health'),
+        ('S', 'School'),
+        ('O', 'Others'),
+    ]
     title = models.CharField(max_length=200)
     content = models.TextField()
     image = models.ImageField(upload_to='images/')
     created_at = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=BLOG_TYPE, default='')
     
     def __str__(self):
         return self.title
